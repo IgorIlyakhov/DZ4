7685d943a8134820832155309369e60c0db800c0
Ihor n-v <117747499+IgorIlyakhov@users.noreply.github.com>
2022-12-05 17:27:32 +0200

    Вікусік

---
+++ пример.py
@@ -0,0 +1,28 @@
+cheese_roquefort_price = float(12.50)
+cheese_roq_ingridient = "Cheese Roquefort -"
+cheese_stilton_price = float(11.24)
+cheese_stilton_ingridient = "Cheese Stilton -"
+cheese_brie_price = float(9.30)
+cheese_brie_ingridient = "Cheese Brie -"
+cheese_gauda_price = float(8.55)
+cheese_gauda_ingridient = "Cheese Gauda -"
+cheese_edam_price = float(11.00)
+cheese_edam_ingridient = "Cheese Edam -"
+cheese_parmesan_price = float(16.50)
+cheese_parm_ingridient = "Cheese Parmesan -"
+cheese_mozzarella_price = float(14.00)
+cheese_mozz_ingridient = "Cheese Mozzarella -"
+cheese_slovak_price = float(122.32)
+cheese_slov_ingridient = "Slovak sheep's milk cheese -"
+a = "zl"
+print(cheese_roq_ingridient, cheese_roquefort_price, a)
+print(cheese_stilton_ingridient, cheese_stilton_price, a)
+print(cheese_brie_ingridient, cheese_brie_price, a)
+print(cheese_gauda_ingridient, cheese_gauda_price, a)
+print(cheese_edam_ingridient, cheese_edam_price, a)
+print(cheese_parm_ingridient, cheese_parmesan_price, a)
+print(cheese_mozz_ingridient, cheese_mozzarella_price, a)
+print(cheese_slov_ingridient, cheese_slovak_price, a)
+
+
+
